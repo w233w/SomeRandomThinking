4735bb4cceadb6218051e8028ce87842a4541348
Yang Ye <47234759+w233w@users.noreply.github.com>
2023-02-28 14:24:27 +0800

    Update simple_rpg_fight.py
    Add comments

--- simple_rpg_fight.py
+++ simple_rpg_fight.py
@@ -4,12 +4,20 @@ import _thread as thread
 
 
 class game_obj:
-    def __init__(self, name, hp, atk, attR, critP, critD):
+    '''
+    A simple game sprite
+    name, hp, atk: you know the meaning.
+    arrR: attack rate; how many attacks can this object do in one second
+    critC, critical chance; critC = 0.3 meaning 30% chance
+    critD: critical damage; cridD = 0.5 meaning extra 50% damage when crit.
+    '''
+
+    def __init__(self, name, hp, atk, attR, critC, critD):
         self.name = name
         self.hp = hp
         self.atk = atk
         self.attR = attR
-        self.critP = critP
+        self.critP = critC
         self.critD = critD
 
     def __bool__(self) -> bool:
@@ -19,7 +27,7 @@ class game_obj:
         return random.random() < rng_ratio
 
     def _cal_damage(self) -> int:
-        crit = self._type_one_RNG(self.critP)
+        crit = self._type_one_RNG(self.critC)
         if crit:
             DMG = self.atk * (1 + self.critD)
         else:
@@ -35,6 +43,8 @@ class game_obj:
             self.hp = 0
 
     def attack(self, oppo) -> None:
+        '''simgle attack peocess with some log.
+        '''
         DMG = self._cal_damage()
         oppo.get_hit(DMG)
         if oppo:
@@ -43,6 +53,8 @@ class game_obj:
             print(f'{self.name} deal {DMG} damage to {oppo.name}. {oppo.name} is dead.\n', end='')
 
     def fight(self, oppo) -> None:
+        '''keep attack an opponent with time interval (1 / attR) until any sprite die.
+        '''
         if not isinstance(oppo, self.__class__):
             raise TypeError('Target is not attackable')
         while self and oppo:
@@ -50,24 +62,18 @@ class game_obj:
             time.sleep(1/self.attR)
 
 
-class game_group(list):
-    def append(self, obj):
-        if not isinstance(obj, game_obj):
-            raise TypeError('Only game object can append into this group')
-        super().append(obj)
-
-
 player = game_obj('player', 8000, 200, 5, 0.3, 2.5)
 mob = game_obj('dragon', 12000, 800, 1, 0.5, 1)
-group = game_group([player, mob])
+group = [player, mob]
 
 
-# 创建两个线程
+# Create two threads
 try:
     thread.start_new_thread(player.fight, (mob,))
     thread.start_new_thread(mob.fight, (player,))
 except Exception:
     print(Exception.with_traceback())
 
+# This will keep program run until the fight end.
 while all(group):
     pass
