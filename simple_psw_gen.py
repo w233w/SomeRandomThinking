81ac4d9a7ab4c18a80a813233e04a3e863101cd8
Yang Ye <47234759+w233w@users.noreply.github.com>
2023-03-22 16:31:13 +0800

    Update simple_psw_gen.py

--- simple_psw_gen.py
+++ simple_psw_gen.py
@@ -1,10 +1,17 @@
 import hashlib
 import base64
+import sys
 
 
 # A super simple passward generator
 # generated passward contain both upper/lower case english letter, number and special character.
-s = '??????'
-md5 = hashlib.md5(s.encode(encoding='utf-8')).digest()
-b64 = base64.b64encode(hashlib.md5(s.encode(encoding='utf-8')).digest())
-print(b64[:-2].decode())
+def simple_psw_gen(seed: [str, int]) -> str:
+    string = str(seed)
+    md5 = hashlib.md5(string.encode(encoding='utf-8')).digest()
+    b64 = base64.b64encode(md5)
+    return b64[:-2].decode()
+
+if __name__ == '__main__':
+    seed = sys.argv
+    print(seed)
+    print(simple_psw_gen(seed))
