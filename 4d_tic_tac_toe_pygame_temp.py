77b8d91359de22831a074bd2c73dc3b603bd8d2c
Yang Ye <47234759+w233w@users.noreply.github.com>
2023-03-31 17:39:04 +0800

    Update 4d_tic_tac_toe_pygame_temp.py

--- 4d_tic_tac_toe_pygame_temp.py
+++ 4d_tic_tac_toe_pygame_temp.py
@@ -2,11 +2,12 @@
 Unfinished
 TODO:
     1. Game logic to force player play on specific board
-       or anyplace if on special case
+       or anyplace if on special case               ------- Done
     2. Visual effect to let player know where they can play next
-    3. Simple UI let player click start game buttom to start
-    4. Simple UI let player click info buttom to read the game rule
+    3. Simple UI let player able to click (re)start ------- Half Done, Black start screen now.
+    4. Simple UI let player click info buttom to read the game rule --Maybe put the rule on start screen
     5. Simple UI let player can see who will play next
+    6. Too many magical number, clear the code.
 '''
 import pygame
 from pygame import Vector2
@@ -52,6 +53,31 @@ class utils:
         y_in = sprite_y - half_size <= y <= sprite_y + half_size
         return x_in and y_in
 
+    @staticmethod
+    def stage_update(game_info, groupsingle):
+        if groupsingle.sprite is None:
+            groupsingle.add(UI(Vector2(WIDTH/2, HEIGHT/2)))
+            state_code = -2
+            print('init on ui')
+        elif isinstance(groupsingle.sprite, UI):
+            if game_info['state_code'] == -2:
+                groupsingle.add(LargeChessBoard(Vector2(WIDTH/2, HEIGHT/2)))
+                print('change to board')
+                game_info['state_code'] = 0
+        elif isinstance(groupsingle.sprite, LargeChessBoard):
+            if game_info['state_code'] != 0:
+                groupsingle.add(UI(Vector2(WIDTH/2, HEIGHT/2)))
+                print('change to ui')
+                game_info['state_code'] = 0
+
+    @staticmethod
+    def mouse_event_handler(event_list, event_type, button):
+        for event in event_list:
+            if event.type == event_type:
+                if event.button == button:
+                    return True
+        return False
+            
 
 class ChessBoardUnit(pygame.sprite.Sprite):
     def __init__(self, abs_pos, parent):
@@ -70,15 +96,16 @@ class ChessBoardUnit(pygame.sprite.Sprite):
         self.pos = 136 * parent.abs_pos + 44 * abs_pos + Vector2(image_size/2)
         self.rect = self.image.get_rect(center=self.pos)  # 预设40*40, Unit间隔4
 
-    def update(self):
+    def update(self, game_info, event_list):
         if not self.active:
             return
-        if utils.check_mouse_in_spirte(self.pos, self.image.get_size()[0]):
-            if pygame.mouse.get_pressed()[0] == 1 and self.parent.parent.playable:
-                self.parent.parent.playable = False
-                self.parent.parent.current_player = 3 - self.parent.parent.current_player
-                self.status = self.parent.parent.current_player
-                self.parent.unit_status[int(self.abs_pos.x)][int(self.abs_pos.y)] = self.status
+        if self.parent.playable:
+            if utils.check_mouse_in_spirte(self.pos, self.image.get_size()[0]):
+                if utils.mouse_event_handler(event_list, pygame.MOUSEBUTTONDOWN, 1):
+                    game_info['player'] = 3 - game_info['player']
+                    self.status = game_info['player']
+                    self.parent.unit_status[int(self.abs_pos.x)][int(self.abs_pos.y)] = self.status
+                    self.parent.parent.next_board = self.abs_pos
         self.active = self.status == 0
         self.image = self.image_collection[self.status]
 
@@ -91,6 +118,7 @@ class ChessBoard(pygame.sprite.Sprite):
         self.status = 0
         self.unit_status = np.zeros((3, 3), dtype=np.int32)
         self.active = True
+        self.playable = True
         self.image_collection = {
             0: pygame.image.load("board.png"),  # change to 3*3 grid
             1: pygame.image.load("big-circle.png"),  # change to BIG O
@@ -106,10 +134,17 @@ class ChessBoard(pygame.sprite.Sprite):
             for j in range(3):
                 self.units.add(ChessBoardUnit(Vector2(i, j), self))
 
-    def update(self):
+    def update(self, game_info, event_list):
         if not self.active:
             return
-        self.units.update()
+        if self.parent.rule == 'strict':
+            if self.abs_pos != self.parent.next_board:
+                self.playable = False
+            else:
+                self.playable = True
+        else:
+            self.playable = True
+        self.units.update(game_info, event_list)
         self.units.draw(screen)
         winner = utils.check_winner(self.unit_status)
         self.status = winner
@@ -125,26 +160,33 @@ class LargeChessBoard(pygame.sprite.Sprite):
         self.unit_status = np.zeros((3, 3), dtype=np.int32)
         self.image = pygame.image.load("bomb.png")
         self.rect = self.image.get_rect(center=self.pos)
-        self.game_play = True  # 有开始界面了之后改成False
-        self.current_player = 1
-        self.playable = True
         self.units = pygame.sprite.Group()
         for i in range(3):
             for j in range(3):
                 self.units.add(ChessBoard(Vector2(i, j), self))
-        self.winner = -1
+        self.rule = 'strict'
+        self.next_board = Vector2(1, 1)
 
-    def update(self):
-        if not self.game_play:
-            return
-        self.units.update()
-        if pygame.mouse.get_pressed()[0] == 0 and not self.playable:
-            self.playable = True
-        winner = utils.check_winner(self.unit_status)
-        if winner != 0:
-            print('winner is ', winner)
-            self.game_play = False
+    def update(self, game_info, event_list):
+        if self.unit_status[int(self.next_board.x)][int(self.next_board.y)] != 0:
+            self.rule = 'free'
+        else:
+            self.rule = 'strict'
+        self.units.update(game_info, event_list)
+        game_info['state_code'] = utils.check_winner(self.unit_status)
         self.units.draw(screen)
+
+
+class UI(pygame.sprite.Sprite):
+    def __init__(self, pos):
+        super().__init__()
+        self.pos = pos
+        self.image = pygame.image.load("bomb.png")
+        self.rect = self.image.get_rect(center=self.pos)
+
+    def update(self, game_info, event_list):
+        if utils.mouse_event_handler(event_list, pygame.MOUSEBUTTONDOWN, 1):
+            game_info['state_code'] = -2
 
 
 if __name__ == '__main__':
@@ -160,20 +202,27 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(SIZE)
     clock = pygame.time.Clock()
 
-    gameboard = LargeChessBoard(Vector2(WIDTH/2, HEIGHT/2))
-    #gg = pygame.sprite.Group()
-    #gg.add(gameboard)
-    gamming = False
+    game = pygame.sprite.GroupSingle()
+    # state code explain:
+    # -2 for not in game (on start screen)
+    # -1 for tie
+    # 0 for game not end yet
+    # 1 for player one win
+    # 2 for player two win
+    game_info = {'state_code': 0, 'player': 1}
     running = True
     while running:
         clock.tick(FPS)
+        utils.stage_update(game_info, game)
         screen.fill(pygame.Color(255, 255, 255))
         # 点×时退出。。
-        for event in pygame.event.get():
+        event_list = pygame.event.get()
+        for event in event_list:
             if event.type == pygame.QUIT:
                 running = False
         # Game loop
-        gameboard.update()
-        # gg.draw(screen)
+        game.update(game_info, event_list)
+        # game.draw(screen)
         pygame.display.flip()
-    pygame.quit()
+
+pygame.quit()
