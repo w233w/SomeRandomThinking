e41bb97c78482c898f4256bae9e10828069c0482
Yang Ye <47234759+w233w@users.noreply.github.com>
2023-04-07 17:38:33 +0800

    Update pygame-tower-defence-pre.py
    测试了地图拖拽的功能，还需要检测边界

--- pygame-tower-defence-pre.py
+++ pygame-tower-defence-pre.py
@@ -4,24 +4,43 @@ from pygame.math import Vector2
 
 WIDTH = 200
 HEIGHT = 200
+SIZE = WIDTH, HEIGHT
 FPS = 60
 
 class Info(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, size):
         super().__init__()
         self.font = pygame.font.Font('simhei.ttf',  30)
-        self.rect = pygame.Rect(20, 20, 180, 180)
-        self.image = pygame.Surface((160, 160))
-        self.image.fill((100, 100, 100))
-        self.text = self.font.render("Cool", False, pygame.Color(255, 255, 255))
-        self.image.blit(self.text, (100, 10))
+        #self.image = pygame.Surface(size)
+        self.image = pygame.image.load("bullet.png")
+        self.image = pygame.transform.scale_by(self.image, 30)
+        self.center = Vector2(size)/2
+        self.rect = self.image.get_rect(center = self.center)
+        #self.image.fill((100, 100, 100))
+        #self.text = self.font.render("Cool", False, pygame.Color(255, 255, 255))
+        #self.image.blit(self.text, (70, 10))
         self.components = pygame.sprite.Group()
         self.sidebar = DataBar()
         self.components.add(self.sidebar)
 
-    def update(self):
-        self.components.update()
-        self.components.draw(self.image)
+        self.mouse_init = self.center
+        self.drag = False
+
+    def update(self, event_list):
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            for event in event_list:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mouse_init = Vector2(pos)
+                    self.drag = True
+                if event.type == pygame.MOUSEBUTTONUP:
+                    self.mouse_init = self.center
+                    self.drag = False
+        if self.drag and pygame.mouse.get_pressed()[0] == 1:
+            delta = Vector2(pos) - self.mouse_init
+            self.mouse_init = Vector2(pos)
+            self.center += delta
+            self.rect = self.image.get_rect(center = self.center)
 
 
 class DataBar(pygame.sprite.Sprite):
@@ -39,15 +58,16 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
     clock = pygame.time.Clock()
 
-    game = pygame.sprite.GroupSingle(Info())
+    game = pygame.sprite.GroupSingle(Info(SIZE))
     running = True
     while running:
         clock.tick(FPS)
+        #screen.fill(pygame.color.Color(255, 255, 255))
         event_list = pygame.event.get()
         for event in event_list:
             if event.type == pygame.QUIT:
                 running = False
-        game.update()
+        game.update(event_list)
         game.draw(screen)
         pygame.display.flip()
 
