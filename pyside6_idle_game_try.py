98e309254c96209ad089df880ace3427a9d1e07a
Yang Ye <47234759+w233w@users.noreply.github.com>
2024-08-30 18:29:14 +0800

    Update pyside6_idle_game_try.py

--- pyside6_idle_game_try.py
+++ pyside6_idle_game_try.py
@@ -8,28 +8,31 @@ from PySide6.QtWidgets import QWidget
 from functools import partial
 from collections import defaultdict
 from copy import deepcopy
-from typing import Callable, TypeAlias, Literal
-from abc import abstractmethod
+from typing import Callable, TypeAlias, Literal, Any
+from abc import abstractmethod, abstractstaticmethod
 
 Resource: TypeAlias = Literal["red", "green", "blue"]
 Status: TypeAlias = dict[Resource, int]
-Buff: TypeAlias = Callable[[Status], Status]
+# Buff: TypeAlias = Callable[[Status], Status]
 Coord: TypeAlias = tuple[int, int]
 
 # Miner: 分颜色，每秒挖颜色矿物1个，储量上限为100
 # Booster: 四角Miner/learner每次多挖一个矿
 # AdvBooster: 四角Miner/learner每次多挖1倍的指定颜色的矿
-# Learner: 四周矿机储量的10%作为自身挖掘速度，任意矿的自身储量大于仓库同类矿的储量时停产。
+# Learner: 上一个出售的Miner的储量的10%作为自身挖掘速度，任意矿的自身储量大于仓库同类矿的储量时停产。
 # Market: 每秒扣除仓库中各种矿1个，生产1gold
-# Sender: 八方向生产者每秒上传所有矿一个至仓库
+# Sender: 四周生产者每秒上传所有矿一个至仓库
 
-# 效果怎么做：
-# Miner: 每帧向Controller发送{Coord:Buff}: Buff是10%的自身资源量
-# *Boooster: 每帧向Controller发送{Coord：Buff}： Buff是全种类矿物+1
-# Learner: 不发送Buff
-# Market/Sender: 不发送Buff？？？
 
-# Buff 通过设计优先级，实现先加减后乘除
+class Buff:
+    def __init__(self, name: str, p: int, callback: Callable[[Status], Status]) -> None:
+        self.name = name
+        self.p = p
+        self.callback = callback
+
+    def apply(self, target: Status) -> Status:
+        return self.callback(target)
+
 
 class Building:
     def __init__(self, name: str, coord: Coord) -> None:
@@ -39,40 +42,38 @@ class Building:
     def display_info(self) -> tuple[bool, str]:
         return ""
 
-    def offer_buff(self) -> dict[Coord, Buff]:
-        return {}
-    
-class Producer(Building):
-    def __init__(self, name: str, coord: tuple[int, int]) -> None:
-        super().__init__(name, coord)
-    
-    @abstractmethod
-    def produce(self, buffs: list[Buff]): ...
-    
-    
-class Buffer(Building):
-    def __init__(self, name: str, coord: tuple[int, int]) -> None:
-        super().__init__(name, coord)
-    
+    @abstractstaticmethod
+    def tooltip() -> str: ...
     @abstractmethod
     def offer_buff(self) -> dict[Coord, Buff]: ...
+    @abstractmethod
+    def update(self, buffs: list[Buff]) -> None: ...
 
 
-class DefaultMiner(Producer):
-    def __init__(self, name, coord) -> None:
+class Miner(Building):
+    def __init__(self, name: str, coord: Coord) -> None:
         super().__init__(name, coord)
         self.resource: dict[Resource, int] = {
             "red": 0,
             "green": 0,
             "blue": 0,
         }
+        self.time = 0
         self.base_status: Status = {
             "red": 1,
             "green": 1,
             "blue": 1,
         }
-        self.time = 0
-        self.time_per_update = 25
+        self.time_per_produce = 25
+        self.affect_range: list[Coord] = list(
+            filter(
+                lambda c: c[0] >= 0 and c[0] <= 4 and c[1] >= 0 and c[1] <= 4,
+                map(
+                    lambda c: (c[0] + self.coord[0], c[1] + self.coord[1]),
+                    ([0, 1], [0, -1], [1, 0], [-1, 0]),
+                ),
+            )
+        )
 
     def display_info(self):
         string = f"""<font color=red>{self.resource['red']}</font>
@@ -80,66 +81,111 @@ class DefaultMiner(Producer):
             <font color=blue>{self.resource['blue']}</font>"""
         return True, string
 
+    def tooltip():
+        return "Mine every resource 1 per second."
+
+    def offer_buff(self) -> dict[tuple[int, int], Buff]:
+        def buff(st: Status):
+            st["red"] += int(self.resource["red"] * 0.1)
+            st["green"] += int(self.resource["green"] * 0.1)
+            st["blue"] += int(self.resource["blue"] * 0.1)
+            return st
+
+        buffs: dict[Coord, Buff] = {}
+        for c in self.affect_range:
+            buffs[c] = Buff("miner", 1, buff)
+        return buffs
+
     def update(self, buffs: list[Buff]):
         self.time += 1
-        if self.time % self.time_per_update == 0:
+        if self.time % self.time_per_produce == 0:
             status = deepcopy(self.base_status)
+            buffs.sort(key=lambda buff: buff.p)
             for buff in buffs:
-                status = buff(status)
+                status = buff.apply(status)
             for k, v in status.items():
-                if k in self.resource:
-                    self.resource[k] += v
+                self.resource[k] += v
 
 
-class Booster(Buffer):
-    def __init__(self, name, coord) -> None:
+class Booster(Building):
+    def __init__(self, name: str, coord: Coord) -> None:
         super().__init__(name, coord)
-        self.affact_range = list(
-            map(
-                lambda c: (c[0] + self.coord[0], c[1] + self.coord[1]),
-                (product([1, -1], [1, -1])),
+        self.affect_range = list(
+            filter(
+                lambda c: c[0] >= 0 and c[0] <= 4 and c[1] >= 0 and c[1] <= 4,
+                map(
+                    lambda c: (c[0] + self.coord[0], c[1] + self.coord[1]),
+                    (product([1, -1], [1, -1])),
+                ),
             )
         )
 
     def display_info(self):
         return False, "Booster"
 
+    def tooltip():
+        return "四角的生产者+1"
+
     def offer_buff(self) -> dict[Coord, Buff]:
-        def callback(dictionary):
-            dictionary["red"] += 1
-            dictionary["green"] += 1
-            dictionary["blue"] += 1
-            return dictionary
+        def buff(status: Status):
+            status["red"] += 1
+            status["green"] += 1
+            status["blue"] += 1
+            return status
 
         buffs: dict[Coord, Buff] = {}
-        for c in self.affact_range:
-            if c[0] >= 0 and c[0] <= 4 and c[1] >= 0 and c[1] <= 4:
-                buffs[c] = callback
+        for c in self.affect_range:
+            buffs[c] = Buff("booster", 1, buff)
         return buffs
 
+    def update(self, *useless):
+        pass
 
-class Learner(Producer):
-    def __init__(self, name, coord) -> None:
+
+class Learner(Building):
+    def __init__(self, name: str, coord: Coord) -> None:
         super().__init__(name, coord)
-        self.affact_range = list(
-            map(
-                lambda c: (c[0] + self._coord[0], c[1] + self._coord[1]),
-                (product([1, -1], [1, -1])),
-            )
-        )
-        self.resource: dict[Resource, int] = defaultdict(int)
-        self.base_status: Status = defaultdict(int)
+        self.resource: dict[Resource, int] = {
+            "red": 0,
+            "green": 0,
+            "blue": 0,
+        }
+        self.time = 0
+        self.base_status: Status = {
+            "red": 0,
+            "green": 0,
+            "blue": 0,
+        }
+        self.time_per_produce = 25
+
+    def display_info(self):
+        string = f"""<font color=red>{self.resource['red']}</font><br />
+            <font color=green>{self.resource['green']}</font><br />
+            <font color=blue>{self.resource['blue']}</font>"""
+        return True, string
+
+    def tooltip():
+        return "Mine every resource 1 per second."
+
+    def offer_buff(self) -> dict[tuple[int, int], Buff]:
+        return {}
+
+    def update(self, buffs: list[Buff]):
+        self.time += 1
+        if self.time % self.time_per_produce == 0:
+            status = deepcopy(self.base_status)
+            buffs.sort(key=lambda buff: buff.p)
+            for buff in buffs:
+                if buff.name in ["miner"]:
+                    status = buff.apply(status)
+            for k, v in status.items():
+                self.resource[k] += v
 
 
 class ShopItem:
-    info: dict[str, type[Building]] = {"Base": DefaultMiner, "Booster": Booster}
-    tooltips: dict[str, str] = {
-        "Base": "Mine every resource 1 per second.",
-        "Booster": "Booster",
-    }
-
     def __init__(self, name: str, sale_price: int) -> None:
         self.name: str = name
+        self.building: Building = globals()[self.name]
         self.sale_price: int = sale_price
 
     @property
@@ -148,28 +194,24 @@ class ShopItem:
 
     @property
     def tooltip(self) -> str:
-        return ShopItem.tooltips[self.name]
+        return self.building.tooltip()
 
-    def on_sale(self, coord) -> Building:
-        m = ShopItem.info[self.name]
-        return m(self.name, coord)
+    def on_sale(self, coord: Coord) -> Building:
+        return self.building(self.name, coord)
 
 
 class GameController:
     def __init__(self) -> None:
         self.gold = 10000
-        self.resources = {
-            "red": 1,
-            "green": 1,
-            "blue": 1
-        }
+        self.resources: Resource = {"red": 0, "green": 0, "blue": 0}
         self.coords = product(range(5), range(5))
         self.grids: dict[Coord, Building] = {coord: None for coord in self.coords}
         self.selected: Coord = (-1, -1)
 
         self.shop_items: list[ShopItem] = [
-            ShopItem("Base", 0),
+            ShopItem("Miner", 0),
             ShopItem("Booster", 10),
+            ShopItem("Learner", 10),
         ]
 
         self.global_buff_collection: dict[Coord, list[Buff]] = defaultdict(list)
@@ -188,10 +230,16 @@ class GameController:
         self.grids[self.selected] = self.shop_items[index].on_sale(self.selected)
         self.gold -= self.shop_items[index].sale_price
         self.selected = (-1, -1)
-        
+
     def on_sale(self):
         if self.selected == (-1, -1):
             return
+        if hasattr(self.grids[self.selected], "resource"):
+            self.resources["red"] += self.grids[self.selected].resource["red"]
+            self.resources["green"] += self.grids[self.selected].resource["green"]
+            self.resources["blue"] += self.grids[self.selected].resource["blue"]
+        self.grids[self.selected] = None
+        self.selected = (-1, -1)
 
     def on_select(self, coord):
         if self.selected != coord:
@@ -210,14 +258,13 @@ class GameController:
                 if c1 in self.grid_not_None:
                     self.global_buff_collection[c1].append(buff)
         for c in self.grid_not_None:
-            self.grids[c].update(self.global_buff_collection[c])
+            self.grids[c].update(self.global_buff_collection.get(c, []))
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Test")
-        self.statusBar().clearMessage()
 
         # load key data
         self.game = GameController()
@@ -264,7 +311,7 @@ class MyWidget(QMainWindow):
         for i in range(5):
             for j in range(5):
                 self.main_grid_items[(i, j)] = QPushButton(self.main_grid)
-                self.main_grid_items[(i, j)].setMinimumSize(60, 60)
+                self.main_grid_items[(i, j)].setMinimumSize(80, 80)
                 self.main_grid_items[(i, j)].clicked.connect(
                     partial(self.game.on_select, (i, j))
                 )
@@ -278,18 +325,19 @@ class MyWidget(QMainWindow):
         self.shop_items: list[QPushButton] = []
         for i in range(len(self.game.shop_items)):
             self.shop_items.append(QPushButton())
-            self.shop_items[-1].setMaximumHeight(60)
+            self.shop_items[-1].setMaximumHeight(80)
             self.shop_items[-1].clicked.connect(partial(self.game.on_shop, i))
             self.shop_items[-1].setText(self.game.shop_items[i].sale_info)
             self.shop_items[-1].setToolTip(self.game.shop_items[i].tooltip)
             self.shop_layout.addWidget(self.shop_items[-1])
-            
+
         # 右上方出售按钮
         self.sale = QGroupBox("Sale")
         self.sale_layout = QVBoxLayout(self.sale)
         self.sale_button = QPushButton("Sale\nSelected", self.sale)
-        self.sale_layout.addWidget(self.sale_button)    
-        
+        self.sale_button.clicked.connect(self.game.on_sale)
+        self.sale_layout.addWidget(self.sale_button)
+
         # 右中下侧升级布局
         self.upgrade = QGroupBox("Upgrade")
         self.upgrade_layout = QVBoxLayout(self.upgrade)
@@ -301,7 +349,6 @@ class MyWidget(QMainWindow):
         self.global_layout.addWidget(self.shop, 2, 0)
         self.global_layout.addWidget(self.sale, 0, 1)
         self.global_layout.addWidget(self.upgrade, 1, 1, 2, 1)
-        
 
         self.setCentralWidget(self.global_widget)
 
@@ -313,9 +360,9 @@ class MyWidget(QMainWindow):
     def global_update(self):
         self.game.update()
         self.info_gold.setText(str(self.game.gold))
-        self.info_red.setText(str(self.game.resources['red']))
-        self.info_green.setText(str(self.game.resources['green']))
-        self.info_blue.setText(str(self.game.resources['blue']))
+        self.info_red.setText(str(self.game.resources["red"]))
+        self.info_green.setText(str(self.game.resources["green"]))
+        self.info_blue.setText(str(self.game.resources["blue"]))
         for coord in product(range(5), range(5)):
             if self.game.is_selected(coord):
                 self.main_grid_items[coord].setStyleSheet(
