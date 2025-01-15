5871ff84e6e2a8e46bd893239bc5d0ba0aeea535
Yang Ye <47234759+w233w@users.noreply.github.com>
2025-01-15 18:06:47 +0800

    Update none_push_box.py
    final ver

--- none_push_box.py
+++ none_push_box.py
@@ -1,11 +1,11 @@
 import numpy as np
 from enum import IntEnum
-import keyboard
-import os
+from pynput.keyboard import Key, KeyCode, Listener, Controller
+from rich.console import Console
+from rich.style import Style
 
 
 class T(IntEnum):
-    N = 0  # none
     W = 1  # wall
     B = 2  # box
     T = 4  # target
@@ -14,63 +14,91 @@ class T(IntEnum):
 
 board = np.array(
     [
-        [1, 1, 1, 1, 1, 1],
-        [1, 8, 0, 0, 0, 1],
-        [1, 0, 2, 0, 4, 1],
-        [1, 0, 1, 2, 4, 1],
-        [1, 0, 2, 0, 4, 1],
-        [1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 4, 0, 0, 8, 1],
+        [1, 0, 0, 2, 2, 2, 0, 0, 1],
+        [1, 4, 1, 1, 4, 1, 1, 4, 1],
+        [1, 0, 0, 0, 2, 0, 0, 0, 1],
+        [1, 0, 0, 2, 4, 1, 0, 1, 1],
+        [1, 1, 1, 1, 0, 0, 0, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1],
     ]
 )
 assert ((board & T.P) == T.P).sum() == 1
 assert ((board & T.B) == T.B).sum() == ((board & T.T) == T.T).sum()
-os.system("cls")
+
+controller = Controller()
+console = Console()
+console.clear()
+colors = {
+    T.W: Style(color="cyan", bgcolor="cyan"),
+    T.B: Style(color="green", bgcolor="green"),
+    T.T: Style(color="red", bgcolor="red"),
+    T.P: Style(color="blue", bgcolor="blue"),
+    T.B | T.T: Style(color="yellow", bgcolor="yellow"),
+    T.P | T.T: Style(color="purple", bgcolor="purple"),
+}
 
 
-def on_action(press_event: keyboard.KeyboardEvent):
-    # os.system("cls")
+def rich_print(board: np.ndarray):
+    for row in board:
+        for value in row:
+            if value in colors:
+                styled_value = f"[{colors[value]}]  [/]"
+            else:
+                styled_value = "  "
+            console.print(styled_value, end="")
+        console.print()
+
+
+def on_action(key: Key | KeyCode):
     p_pos = board.argmax()
     b = board.ravel()
-    match press_event.name:
-        case "w":
+    console.clear()
+    if isinstance(key, KeyCode):
+        btn = key.char
+    elif isinstance(key, Key):
+        btn = key.name
+    match btn:
+        case "w" | "up":
             step = -board.shape[1]
             n_in_front = p_pos // abs(step)
-        case "s":
+        case "s" | "down":
             step = board.shape[1]
             n_in_front = (board.shape[0] * board.shape[1] - 1 - p_pos) // abs(step)
-        case "a":
+        case "a" | "left":
             step = -1
             n_in_front = p_pos % board.shape[1]
-        case "d":
+        case "d" | "right":
             step = 1
             n_in_front = board.shape[1] - p_pos % board.shape[1] - 1
         case "q":
-            return
+            return False
         case _:
-            print("invalid")
+            console.clear()
+            rich_print(board)
+            console.print("invalid")
             return
     sight = [b[p_pos + i * step] for i in range(1, n_in_front + 1)]
     if sight[0] & T.W == T.W:  # hit wall
         pass
     elif sight[0] & T.B == T.B:  # hit box
-        if sight[1] & T.W != T.W and sight[1] & T.B != T.B:  # nothing behind box
+        if sight[1] & T.W != T.W and sight[1] & T.B != T.B:  # nothing block box
             b[p_pos] -= T.P
             b[p_pos + step] += T.P
             b[p_pos + step] -= T.B
             b[p_pos + 2 * step] += T.B
-        else:  # sonething behind the box
+        else:  # something block the box
             pass
     else:  # hit nothing
         b[p_pos] -= T.P
         b[p_pos + step] += T.P
-    print(board)
-    if ((board & (T.B | T.T)) == (T.B | T.T)).sum() == ((board & T.B) == T.B).sum():
-        print("WIN")
-        return
+    rich_print(board)
+    if (board == T.B).sum() == 0:
+        console.print("WIN")
+        return False
 
 
-if __name__ == f"__main__":
-    print(board)
-    keyboard.on_press(on_action)
-    keyboard.wait("q", True)
-    os.system("cls")
+with Listener(on_press=on_action, suppress=True) as l:  # type: ignore
+    rich_print(board)
+    l.join()
